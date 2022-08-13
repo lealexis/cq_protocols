c96989c116acb73da86f230f1b36bbd93b20a35c
lealexis <leonardo_alexis.gonzalez_zuniga@mailbox.tu-dresden.de>
2022-08-13 23:28:51 +0200

    consistency of the methods checked: TODO: storing of information.

--- EPR_distribution_Link/ent_buff_interface_class_proto_v0.9.py
+++ EPR_distribution_Link/ent_buff_interface_class_proto_v0.9.py
@@ -66,7 +66,7 @@ class EPR_buff_itfc(object):
         self.f_IDs = set(np.linspace(0, (self.n-1), self.n, dtype=int))
         self.u_IDs = []
         self.ID_in_process = ([],[])
-        self.is_in_process = False
+        self.in_process = False
         if ideal_FEU is None:
             self.ideal_FEU = True
         else:
@@ -83,7 +83,7 @@ class EPR_buff_itfc(object):
             self.use_max_Fest =  True
         else:
             # use the oldest uID first
-            self.use_max_Fest = False
+            self.use_max_Fest = use_max_Fest
 
     @property
     def start_time(self):
@@ -199,7 +199,7 @@ class EPR_buff_itfc(object):
     def _drop_stored_epr_frame_ID(self, id_f=None):
 
         qids = self._get_qubit_ids_from_fr_ID(id_f=id_f)
-        if self.is_in_process and (id_f in self.ID_in_process[0]):
+        if self.in_process and (id_f in self.ID_in_process[0]):
             self._reset_is_IN_PROCESS()
         if id_f in self.u_IDs:
             self.u_IDs.remove(id_f)
@@ -222,7 +222,7 @@ class EPR_buff_itfc(object):
         if type(mssg) is not str:
             raise ValueError("pass a valid string mssg")
         else:
-            if self.is_in_process and (self._get_MSSG_in_process() == "EPR:storing"):
+            if self.in_process and (self._get_MSSG_in_process() == "EPR:storing"):
                 self.ID_in_process[1].append(mssg)
             else:
                 raise InterfaceIsNotInProcessException("Interface must be "
@@ -230,7 +230,7 @@ class EPR_buff_itfc(object):
 
     def _set_mssg_to_ID_ip(self, mssg=None):
     
-        if self.is_in_process:
+        if self.in_process:
             if len(self.ID_in_process[1]) == 1:
                 self.ID_in_process[1].clear()
                 self.ID_in_process[1].append(mssg)
@@ -262,19 +262,19 @@ class EPR_buff_itfc(object):
         return MSSG_ip
 
     def _reset_is_IN_PROCESS(self):
-        if self.is_in_process:
-            self.is_in_process = False
+        if self.in_process:
+            self.in_process = False
             self.ID_in_process[0].clear()
             self.ID_in_process[1].clear()
         else:
             raise ValueError("No Frame is being processed. Imposible to reset"
-                             " is_in_process.")
+                             " in_process.")
 
     # ***************************************************
     # ** Methods for Phases of EPR-Frame Communication **
     # ***************************************************
     def nxt_fID_EPR_START(self):
-        if not self.is_in_process:
+        if not self.in_process:
             free_list = self.f_IDs.copy()
             free_list = list(free_list)
             nxt_ID = free_list[0]
@@ -282,7 +282,7 @@ class EPR_buff_itfc(object):
             self.f_IDs.remove(nxt_ID)
             self._append_id_to_ID_ip(id_f=nxt_ID)
             self._set_mssg_to_ID_ip(mssg="EPR:started")
-            self.is_in_process = True
+            self.in_process = True
             return nxt_ID
         else:
             raise InterfaceIsInProcessException("EPR_START cannot be called. "
@@ -295,7 +295,7 @@ class EPR_buff_itfc(object):
             raise BadInputException("A valid epr_half must be passed.")
         mssg = self._get_MSSG_in_process()
         valid_mssg = (mssg == "EPR:started") or (mssg == "EPR:storing") 
-        if self.is_in_process and valid_mssg: 
+        if self.in_process and valid_mssg: 
             qids = self._get_qubit_ids_from_fr_ID(id_f=self._get_ID_in_process())
             q_id = self.host.add_epr(host_id=self.partner_host_id, qubit=epr_half)        
             if len(qids)==0:
@@ -307,7 +307,7 @@ class EPR_buff_itfc(object):
         
     def get_qids_EPR_PHASE_2(self):
         
-        if self.is_in_process and self._get_MSSG_in_process() == "EPR:storing":
+        if self.in_process and self._get_MSSG_in_process() == "EPR:storing":
             qids = self._get_qubit_ids_from_fr_ID(id_f=self._get_ID_in_process())
             self._set_mssg_to_ID_ip("EPR:FEU-measuring")
             qubit_ids = qids.copy()
@@ -322,7 +322,7 @@ class EPR_buff_itfc(object):
 
         if type(qubit_id) is not str:
             raise BadInputException("A valid qubit_id must be passed.")
-        if self.is_in_process and self._get_MSSG_in_process() == "EPR:FEU-measuring":
+        if self.in_process and self._get_MSSG_in_process() == "EPR:FEU-measuring":
             qids = self._get_qubit_ids_from_fr_ID(id_f=self._get_ID_in_process())
             try:
                 idx = qids.index(qubit_id)
@@ -339,35 +339,49 @@ class EPR_buff_itfc(object):
                                         " after EPR_PHASE_2, *UNTIL* all of the"
                                         " qubit to me measured were retrieved.")
 
-    # only valid for FIRST version of the protocol
-    def reset_itfc_EPR_END_PHASE_proto_V1(self):
+    # reaction to ACK
+    def set_F_est_EPR_END_PHASE(self, F_est=None):
+        if self.in_process and self._get_MSSG_in_process == "EPR:FEU-validating":
+            ip_ID = self._get_ID_in_process()
+            f = self._get_F_est_from_fr_ID(id_f=ip_ID)
+            if len(f)==0:
+                f.append(F_est)
+                if len(self.u_IDs)==0:
+                    self.is_empty = False
+                self.u_IDs.append(ip_ID)
+                if len(self.u_IDs)==self.n:
+                    self.is_full = True
+                self._reset_is_IN_PROCESS()
+            else:
+                raise FidelitySettingException("Frame {iD} has already a f_est.".format(iD=ip_ID))
+        else:
+            raise BadPhaseCallException("set_F_est_EPR_END_PHASE can be called "
+                                        "only after EPR_PHASE_3.")
 
-        if self.is_in_process and self._get_MSSG_in_process=="EPR:storing":
+    # NACK reaction
+    def drop_ip_frame_EPR_END_PHASE(self):
+
+        if self.in_process and self._get_MSSG_in_process=="EPR:FEU-validating":
+            self._drop_stored_epr_frame_ID(id_f=self._get_ID_in_process())
+        else:
+            raise BadPhaseCallException("drop_ip_frame_EPR_END_PHASE can be "
+                                        "called only after EPR_PHASE_3.")
+
+    # reaction to reset
+    def reset_itfc_EPR_END_PHASE(self):
+
+        if self.in_process and (self._get_MSSG_in_process=="EPR:storing" or 
+                                self._get_MSSG_in_process=="EPR:FEU-validating"):
             for frame_id in self.buffer_info:
                 self._drop_stored_epr_frame_ID(id_f=frame_id)
         else:
             raise BadPhaseCallException("reset_itfc_EPR_END_PHASE can be called"
-                                        " only after EPR_PHASE_ONE.")
+                                        " only after EPR_PHASE_ONE or EPR_PHASE_3.")
     
     # reaction to D-NACK 
-    def drop_DNACK_EPR_END_PHASE(self):
-        
-        if self.is_in_process and self._get_MSSG_in_process=="EPR:storing":
-            uID = None
-            if self.use_max_Fest:
-                uID = self._get_uID_with_max_Fest()
-            else:
-                uID = self._get_oldest_uID()
-            self._drop_stored_epr_frame_ID(id_f=uID)
-            self._drop_stored_epr_frame_ID(id_f=self._get_ID_in_process())
-        else:
-            raise BadPhaseCallException("drop_DNACK_EPR_END_PHASE can be called"
-                                        " only after EPR_PHASE_ONE.")
-    
-    # reaction to D-NACK version 1
-    def drop_DNACK_EPR_END_PHASE_proto_V1(self, rec_uID=None, verbose=False):
+    def drop_DNACK_EPR_END_PHASE(self, rec_uID=None, verbose=False):
 
-        if self.is_in_process and self._get_MSSG_in_process=="EPR:storing": 
+        if self.in_process and self._get_MSSG_in_process=="EPR:storing": 
             if rec_uID is None:
                 raise ValueError("Pass a valid rec_uID to be dropped")
             else:
@@ -389,38 +403,10 @@ class EPR_buff_itfc(object):
             raise BadPhaseCallException("drop_DNACK_EPR_END_PHASE can be called"
                                         " only after EPR_PHASE_ONE.")
 
-    # NACK reaction
-    def drop_ip_frame_EPR_END_PHASE(self):
-
-        if self.is_in_process and self._get_MSSG_in_process=="EPR:FEU-validating":
-            self._drop_stored_epr_frame_ID(id_f=self._get_ID_in_process())
-        else:
-            raise BadPhaseCallException("drop_ip_frame_EPR_END_PHASE can be "
-                                        "called only after EPR_PHASE_3.")
-
-    # ACK
-    def set_F_est_EPR_END_PHASE(self, F_est=None, frame_id=None):
-        if self.is_in_process and self._get_MSSG_in_process == "EPR:FEU-validating":
-            ip_ID = self._get_ID_in_process()
-            f = self._get_F_est_from_fr_ID(id_f=ip_ID)
-            if len(f)==0:
-                f.append(F_est)
-                if len(self.u_IDs)==0:
-                    self.is_empty = False
-                self.u_IDs.append(ip_ID)
-                if len(self.u_IDs)==self.n:
-                    self.is_full = True
-                self._reset_is_IN_PROCESS()
-            else:
-                raise FidelitySettingException("Frame {iD} has already a f_est.".format(iD=ip_ID))
-        else:
-            raise BadPhaseCallException("set_F_est_EPR_END_PHASE can be called "
-                                        "only after EPR_PHASE_3.")
-    
     # correct EPR-Frame in storing process as SDC-Frame and decode c-info.
     def nxt_uID_CORRECT_epr_as_sdc_EPR_PHASE_2(self):
 
-        if self.is_in_process and (self._get_MSSG_in_process() == "EPR:storing"):
+        if self.in_process and (self._get_MSSG_in_process() == "EPR:storing"):
             nxt_ID = None
             if self.use_max_Fest:
                 nxt_ID = self._get_uID_with_max_Fest()
@@ -432,28 +418,29 @@ class EPR_buff_itfc(object):
         else:
             raise BadPhaseCallException("nxt_uID_CORRECT_epr_as_sdc_EPR_PHASE_2"
                                         " can be called only after EPR_PHASE_1.")
-            
-    # valid for FIRST and SECOND version of the protocol
-    def pop_sync_SDC_END_PHASE(self, rcv_ID=None, sto_ID=None):
+ 
+    def pop_sync_SDC_END_PHASE(self):
         """Used after using *store_epr_fID_EPR_PHASE_1* and *nxt_uID_SDC_START*
         in order to correct a received SDC-Frame misinterpreted as EPR-Frame. 
         The received SDC-Frame was stored as EPR-Frame. The received and stored
         SDC-Frame *rcv_ID* is poped alongside its corresponding stored EPR-Frame 
         *sto_ID* in order to decode the SDC-encoded classical information."""
         
-        if (self.is_in_process and ((self._get_MSSG_in_process()[1]=="SDC-correct_EPR") or
-                                    self._get_MSSG_in_process()[1]=="SDC-sync rtrv sto")):
+        ip_MSSGs = self._get_MSSG_in_process() 
+        mssg_is_valid = ((ip_MSSGs[1]=="SDC:correct_EPR") or (
+                                            ip_MSSGs[1]=="SDC:sync_rtrv_sto"))
+        if self.in_process and mssg_is_valid:
             ip_IDs = self._get_ID_in_process()
-            ip_MSSGs =  self._get_MSSG_in_process()
+           
             m1 = (ip_MSSGs[0] == "EPR:storing")
-            m2 = (ip_MSSGs[1] == "SDC-correct_EPR")
+            m2 = (ip_MSSGs[1] == "SDC:correct_EPR")
 
             if (len(ip_IDs) == len(ip_MSSGs) == 2):
                 rcv_qids = self._get_qubit_ids_from_fr_ID(id_f=ip_IDs[0])
                 sto_qids = self._get_qubit_ids_from_fr_ID(id_f=ip_IDs[1])
                 if m1 and m2: 
                     if len(rcv_qids) == len(sto_qids) == self.eff_load:
-                        self._set_mssg_to_ID_ip(["SDC-sync rtrv rec", "SDC-sync rtrv sto"])
+                        self._set_mssg_to_ID_ip(["SDC:sync_rtrv_rec", "SDC:sync_rtrv_sto"])
                     else:
                         raise ValueError("Sync retrieving of pairs: frames has not"
                                          " the same amount of pairs.")
@@ -462,12 +449,12 @@ class EPR_buff_itfc(object):
                 rcv_half = self.host.get_epr(host_id=self.partner_host_id, q_id=rcv_qid)
                 sto_half = self.host.get_epr(host_id=self.partner_host_id, q_id=sto_qid)
                 if len(rcv_qids) == len(sto_qids) == 0:
-                    self._clear_frame_info(frame_id=rcv_ID)
-                    self._clear_frame_info(frame_id=sto_ID)
+                    self._clear_frame_info(frame_id=ip_IDs[0])
+                    self._clear_frame_info(frame_id=ip_IDs[1])
                     if len(self.f_IDs)==0:
                         self.is_full = False
-                    self.f_IDs.add(rcv_ID)
-                    self.f_IDs.add(sto_ID)
+                    self.f_IDs.add(ip_IDs[0])
+                    self.f_IDs.add(ip_IDs[1])
                     if len(self.f_IDs)==self.n:
                         self.is_empty = True
                     self._reset_is_IN_PROCESS()
@@ -481,64 +468,89 @@ class EPR_buff_itfc(object):
     # ***************************************************
     # ** Methods for Phases of SDC-Frame Communication **
     # ***************************************************
-    
-    """ The implemented methods are for the second version of the protocol, 
-    unless they have an explicit *_proto_V1* trailing end on their name"""
 
-    # SDC_START in FIRST and SECOND version of protocol
     def nxt_uID_SDC_START(self):
-        
-        nxt_ID = None
-        if self.use_max_Fest:
-            nxt_ID = self._get_uID_with_max_Fest()
-        else:
-            nxt_ID = self._get_oldest_uID()
-        if self.is_in_process:
-            self.ID_in_process.update({nxt_ID:"SDC-correct_EPR"})
-        else:
-            self.is_in_process = True
-            self.ID_in_process = {nxt_ID:"SDC-started"}
-        return nxt_ID
-    
+        if not self.in_process:
+            nxt_ID = None
+            if self.use_max_Fest:
+                nxt_ID = self._get_uID_with_max_Fest()
+            else:
+                nxt_ID = self._get_oldest_uID()
 
-    # SDC_PHASE_1 in First protocol version
-    def pop_epr_uID_SDC_END_PHASE(self, frame_id=None):
-        
-        qids = self._get_qubit_ids_from_fr_ID(id_f=frame_id)
-        if len(qids)==self.eff_load:
-            self.ID_in_process = {frame_id:"SDC:epr-retrieving"}
-        qid = qids.pop(0)
-        epr_half = self.host.get_epr(host_id=self.partner_host_id, q_id=qid)
-        if len(qids) == 0:
-            self._clear_frame_info(frame_id=frame_id)
-            if len(self.f_IDs)==0:
-                self.is_full = False
-            self.f_IDs.add(frame_id)
+            self._append_id_to_ID_ip(id_f=nxt_ID)
+            self._set_mssg_to_ID_ip(mssg="SDC:started")
+            self.in_process = True
+            return nxt_ID
+        else:
+            raise InterfaceIsInProcessException("SDC_START cannot be called. "
+                                                "Interface is already processing"
+                                                " a frame.")
+
+    def pop_SDC_PHASE_1(self):
+
+        if (self.in_process and ((self._get_MSSG_in_process()=="SDC:started")
+                                    or (self._get_MSSG_in_process()=="SDC:epr-retrieving"))):
+            ip_ID = self._get_ID_in_process()
+            qids = self._get_qubit_ids_from_fr_ID(id_f=ip_ID)
+            if len(qids)==self.eff_load:
+                self._set_mssg_to_ID_ip(mssg="SDC:epr-retrieving")
+            qid = qids.pop(0)
+            epr_half = self.host.get_epr(host_id=self.partner_host_id, q_id=qid)
+            if len(qids) == 0:
+                self._clear_frame_info(frame_id=ip_ID)
+                if len(self.f_IDs)==0:
+                    self.is_full = False
+                self.f_IDs.add(ip_ID)
+                if len(self.f_IDs)==self.n:
+                    self.is_empty = True
+                self._set_mssg_to_ID_ip(mssg="SDC:wait_ACK/NACK")
+            return epr_half
+        else:
+            raise BadPhaseCallException("SDC_END_PHASE can be repeatedly called"
+                                        " only after SDC_START until stored "
+                                        "frame was consumed.")
+
+    # reaction to ACK
+    def ack_SDC_END_PHASE(self):
+        if self.in_process and self._get_MSSG_in_process=="SDC:wait_ACK/NACK":
+            self._reset_is_IN_PROCESS()
+        else:
+            raise BadPhaseCallException("SDC_END_PHASE can be called only after"
+                                        " SDC_PHASE_1.")
+                                        
+    # reaction to NACK
+    def drop_rec_uID_SDC_END_PHASE(self, rec_uID=None, verbose=False):
+        if self.in_process and self._get_MSSG_in_process=="SDC:wait_ACK/NACK":
             
-            if len(self.f_IDs)==self.n:
-                self.is_empty = True
-            self.ID_in_process = {}
-            self.is_in_process = False
-        return epr_half
+            if rec_uID is None:
+                raise ValueError("Pass a valid rec_uID to be dropped")
+            else:
+                uID = self._get_ID_in_process()
+                if verbose:
+                    if uID == rec_uID:
+                        print("next frame to be used was equal at sender and "
+                              "receiver.")
+                    else:
+                        print("next frame to be used was different at sender "
+                              "and receiver.")
+                self._drop_stored_epr_frame_ID(id_f=rec_uID)
+                self._reset_is_IN_PROCESS()
+        else:
+            raise BadPhaseCallException("SDC_END_PHASE can be called only after"
+                                        " SDC_PHASE_1.")
 
-    # NACK: SDC_END_PHASE in FIRST protocol version
-    def drop_uFrame_SDC_END_PHASE_proto_V1(self, frame_id=None):
-        self._drop_stored_epr_frame_ID(id=frame_id)
-    
     # This method is not really needed
-    def get_Fest_and_IDs_by_Fthres(self, f_thres=None):
+    def apply_new_Fthres_to_itfc(self, f_thres=None):
 
-        if (f_thres is None) or (f_thres < 0) or (f_thres > 1):
+        if (f_thres is None) or (f_thres < 0.25) or (f_thres > 1):
             raise ValueError("pass a valid fidelity threshold.")
         
-        above_frames = []
-        bellow_frames = []
-        for fr_id in self.u_IDs:
-            f_est = self._get_F_est_from_fr_ID(id_f=fr_id).copy()
-            if len(f_est)==0:
-                continue
-            if f_est[0] < f_thres:
-                bellow_frames.append((f_est[0], fr_id))
-            else:
-                above_frames.append((f_est[0], fr_id))
-        return above_frames, bellow_frames 
+        if not self.in_process:
+            for fr_id in self.u_IDs:
+                f_est = self._get_F_est_from_fr_ID(id_f=fr_id)
+                if f_est[0] < f_thres:
+                    self._drop_stored_epr_frame_ID(fr_id)
+        else:
+            raise InterfaceIsInProcessException("New threshold fidelity can be "
+                                                "applied only if interface is "
+                                                "not in process.") 
