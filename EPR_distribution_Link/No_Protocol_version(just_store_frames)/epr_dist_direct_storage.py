188824b08160a3957c8b8048f822c35c8bef5dfd
lealexis <leonardo_alexis.gonzalez_zuniga@mailbox.tu-dresden.de>
2022-08-17 19:17:04 +0200

    Most basic example: No ACK/NACK just storing and using frames.

---
+++ EPR_distribution_Link/No_Protocol_version(just_store_frames)/epr_dist_direct_storage.py
@@ -0,0 +1,433 @@
+from qunetsim.components import Network, Host
+from qunetsim.objects import Qubit
+import numpy as np 
+import random
+from PIL import Image
+import QuTils
+import time
+from epr_gen_class import EPR_generator, EPR_Pair_fidelity
+from ent_buff_interface_class import EPR_buff_itfc
+from rotational_error_class import Rot_Error
+from matplotlib import pyplot as plt
+
+DISCRETE_TIME_STEP = 0.1
+FRAME_LENGTH = 35 # Load length
+mean_gamma = np.pi / 16
+channel = True
+
+"""Definition of classical data to be sent through the network. An image 
+is used to be sent over entangled-assisted communication."""
+
+im = Image.open("./Playground_v0.8/mario_sprite.bmp")
+pixels = np.array(im)
+im.close()
+coloumns, rows, colors = pixels.shape
+dtype = pixels.dtype
+
+hashes = []
+hashes_ = []
+
+palette = {}
+indices = {}
+for row in range(rows):
+    for column in range(coloumns):
+        color = pixels[column, row, :]
+        hashed = hash(tuple(color))
+        hashes.append(hashed)
+        hashes_ = hashes.copy()
+        palette[hashed] = color
+
+hashes = list(set(hashes))
+
+for i, hashed in enumerate(hashes):
+    indices[hashed] = i
+
+def binaryTupleFromInteger(i):
+    return tuple([int(j) for j in list(bin(i)[2:].zfill(2))])
+
+def integerFromBinaryTuple(a, b):
+    return a * 2 ** 1 + b * 2 ** 0
+
+# to store the received image
+received_mario = np.zeros((coloumns, rows, colors), dtype=dtype)
+
+global im_2_send 
+im_2_send = ""
+
+for row in range(rows):
+    for column in range(coloumns):
+        color = pixels[column, row, :]
+        hashed = hash(tuple(color))
+        index = indices[hashed]
+        b1, b2 = binaryTupleFromInteger(index)
+        im_2_send += str(b1) + str(b2)
+
+def int2bin(i,length):
+    return [int(j) for j in list(bin(i)[2:].zfill(length))]
+
+def bin2int(bin_str):
+    return int(bin_str,2)
+
+def send_epr_frame(host: Host, receiver_id , epr_gen: EPR_generator, 
+                   qmem_itfc: EPR_buff_itfc, error_gen: Rot_Error ,frm_len=FRAME_LENGTH,  
+                   head_len = 8, verbose=False):
+    frame = []
+    frame_id = qmem_itfc.nxt_fID_EPR_START()
+    for _ in range(head_len):
+        head_qbit = Qubit(host)
+        head_qbit.send_to(receiver_id=receiver_id)
+        
+        error_gen.apply_error(flying_qubit=head_qbit)
+        #QuTils.gaussian_xy_rotation_channel(flying_qubit=head_qbit, mu=mean_gamma)
+        
+        frame.append(head_qbit)
+        time.sleep(DISCRETE_TIME_STEP)
+    f_est_ideal = 0
+    for i in range(frm_len):
+        # TODO: A fraction of the EPR-pairs needs to be measured in order to
+        if verbose:
+            print("Generating EPR pair Nr: {pnum} with Fidelity {F}".format(
+                                                              pnum=i+1, F=Fid))
+        q1, q2 = epr_gen.get_EPR_pair()
+
+        frame.append(q2)
+        f_est_ideal += epr_gen.get_fidelity(half=q1)
+        qmem_itfc.store_EPR_PHASE_1(epr_half=q1)
+
+        #sending the qubit q2 to receiver
+        if verbose:
+            print("Sending EPR halve Nr:{pnum}".format(pnum=i+1))
+        q2.send_to(receiver_id=receiver_id)
+
+        error_gen.apply_error(flying_qubit=q2)
+        #QuTils.gaussian_xy_rotation_channel(flying_qubit=q2, mu=mean_gamma)
+        time.sleep(DISCRETE_TIME_STEP)
+    f_est_ideal = (f_est_ideal / frm_len)
+    qmem_itfc.set_F_est_EPR_END_PHASE(F_est=f_est_ideal, to_history=True)
+    return frame
+
+# im_2_send string to be sent!
+def send_sdense_frame(host:Host, receiver_id, qmem_itfc: EPR_buff_itfc, error_gen: Rot_Error,
+                      frm_len=FRAME_LENGTH, head_len=8, verbose=False):
+    global im_2_send
+    sdense_frame = []
+    frame_id = qmem_itfc.nxt_uID_SDC_START()
+    for _ in range(head_len):
+        head_qbit = Qubit(host)
+        head_qbit.X()# --> header qubit into state "e_1"
+        head_qbit.send_to(receiver_id=receiver_id)
+
+        error_gen.apply_error(flying_qubit=head_qbit)
+        #QuTils.gaussian_xy_rotation_channel(flying_qubit=head_qbit, mu=mean_gamma)
+
+        sdense_frame.append(head_qbit) # control over sent superdense coded frame
+        time.sleep(DISCRETE_TIME_STEP)
+    sent_mssg = ""
+    for mi in range(frm_len):
+        # taking the first two bits & deleting this two bits from im_2_send
+        msg = im_2_send[0:2]
+        im_2_send = im_2_send[2:]
+        sent_mssg += msg
+    
+        qu_i = qmem_itfc.pop_SDC_END_PHASE()
+        if verbose:
+            print("message to be sent is:" + msg)
+        QuTils.dens_encode(qu_i, msg)
+        if verbose:
+            print("Sending superdense coded epr half Nr:{q_i}".format(q_i=mi))
+        qu_i.send_to(receiver_id=receiver_id)
+
+        error_gen.apply_error(flying_qubit=qu_i)
+        #QuTils.gaussian_xy_rotation_channel(flying_qubit=qu_i, mu=mean_gamma)
+        sdense_frame.append(qu_i)
+        time.sleep(DISCRETE_TIME_STEP)
+    return sdense_frame, sent_mssg
+
+def receive_frame(host:Host, qmem_itfc: EPR_buff_itfc, Frame:list,
+                  frm_len=FRAME_LENGTH, head_len = 8,verbose=False):
+    count = 0
+    Type = 0 # 0 means epr frame, 1 means data frame
+    SDC_count = 0
+    EPR_count = 0
+    epr_halves = None 
+    received_mssg = None
+
+    f_est_ideal = 0
+    rec_fr_id = ""
+    frame_id = None
+    for qbit in Frame:
+        if count < head_len:
+            head = qbit.measure()
+            if head == 1: # SDC_count
+                SDC_count += 1
+            else: # EPR_count
+                EPR_count += 1 
+            count += 1
+            #time.sleep(DISCRETE_TIME_STEP)
+            if count == head_len:
+                if SDC_count == EPR_count:
+                    # Neg_ack can not determine if frame is DATA or EPR
+                    # TODO: receiver(host) must communicate failure of the frame
+                    raise Exception("Frame not recognized")
+                    pass
+                elif SDC_count > EPR_count: # Superdense data frame
+                    Type=1
+                    received_mssg = ""
+                    frame_id = qmem_itfc.nxt_uID_SDC_START()
+                    if verbose:      
+                        print("Receiving a Superdense coded data frame")
+                else: # EPR halves frame
+                    if verbose:
+                        print("Receiving an EPR halves frame")
+                    frame_id = qmem_itfc.nxt_fID_EPR_START()
+        else:
+            if Type == 0: # receive epr frame
+                if verbose: 
+                    print("Receiving EPR halve Nr: {hnum}".format(hnum=(count - 
+                                                                head_len + 1)))
+                f_est_ideal += EPR_Pair_fidelity(epr_halve=qbit)
+                qmem_itfc.store_EPR_PHASE_1(epr_half=qbit)
+                time.sleep(DISCRETE_TIME_STEP + 0.035)
+            else: # receive superdense coded data frame
+                retrieved_epr_half = qmem_itfc.pop_SDC_END_PHASE()
+                if retrieved_epr_half is None:
+                    print("Retrieved Pair was None!")
+
+                if qbit.id == retrieved_epr_half.id:
+                    if verbose:
+                        print("Received superdense coded qubit has the same id" 
+                              " as the used epr half!")
+                    decoded_string = QuTils.dense_decode(
+                                            stored_epr_half=retrieved_epr_half, 
+                                            received_qubit=qbit)
+                    received_mssg += decoded_string
+                time.sleep(DISCRETE_TIME_STEP +0.035)
+            count +=1
+            
+    if Type == 0:
+        f_est_ideal = (f_est_ideal / frm_len)
+        qmem_itfc.set_F_est_EPR_END_PHASE(F_est=f_est_ideal)
+
+    if verbose:
+        if ((frm_len + head_len + qmem_itfc.n_exp - 1) == count):
+            print("Frame successfully Received!")
+    if Type == 1:
+        return received_mssg
+    else:
+        return f_est_ideal
+
+def main():
+    start_time = time.time()
+
+    VERBOSE = True
+    save = False
+
+    network = Network.get_instance()
+    
+    Alice = Host('A')
+    Bob = Host('B')
+
+    Alice.add_connection('B')
+    Bob.add_connection('A')
+
+    Alice.start()
+    Bob.start()
+
+    network.add_hosts([Alice, Bob])
+    network.start()
+
+    Alice_EPR_gen =  EPR_generator(host=Alice, min_fid=0.75, max_dev=0.05, 
+                                   f_mu=(1/60), f_sig=(1/30))
+    Alice_EPR_gen.start()
+
+    qumem_itfc_A = EPR_buff_itfc(Alice, Bob.host_id, is_receiver=False)
+    qumem_itfc_A.start_time = Alice_EPR_gen.start_time
+    
+    
+    qumem_itfc_B = EPR_buff_itfc(Bob, Alice.host_id, is_receiver=True)
+    qumem_itfc_B.start_time = Alice_EPR_gen.start_time
+
+    rot_error = Rot_Error(f_sig=0.01)
+    rot_error.start_time = Alice_EPR_gen.start_time
+
+    if VERBOSE:
+        print("Host ALice and Bob started. Network started.")
+
+    choice = ["data", "idle"]
+    
+    comm_length = 200
+
+    sent_mssgs = ""
+    dcdd_mssgs = ""
+
+    if VERBOSE:
+        print("Starting communication loop: Alice sender, Bob receiver.")
+    epr_frame_counter = 0 
+    data_frame_counter = 0
+
+    for step in range(comm_length):
+        state = random.choice(choice)
+
+        if state == "idle": # distribute entanglement
+            epr_frame_counter += 1
+            if VERBOSE:
+                print("IDLE - Alice sending EPR Frame Nr:{nr_eprf}".format(
+                                                    nr_eprf=epr_frame_counter))
+            EPR_frame = send_epr_frame(host=Alice, receiver_id=Bob.host_id, 
+                                       epr_gen=Alice_EPR_gen,
+                                       qmem_itfc=qumem_itfc_A, error_gen=rot_error)
+            if VERBOSE:
+                print("IDLE - Bob receiving EPR Frame Nr:{nr_eprf}".format(
+                                                    nr_eprf=epr_frame_counter))
+            
+            fest = receive_frame(host=Bob, qmem_itfc=qumem_itfc_B, Frame=EPR_frame)
+            qumem_itfc_A.set_F_est_EPR_END_PHASE(F_est=fest)
+        else: # send data frame
+            data_frame_counter += 1
+            if Alice.shares_epr(Bob.host_id): # just send data
+
+                # sending data frame
+                if VERBOSE:
+                    print("COMM - Alice sending Data frame Nr:{df}".format(
+                                                        df=data_frame_counter))
+
+                SDC_frame, classic_mssg = send_sdense_frame(host=Alice, 
+                                                        receiver_id=Bob.host_id,
+                                                        qmem_itfc=qumem_itfc_A,
+                                                        error_gen=rot_error)
+                if VERBOSE:
+                    print("COMM - Alice is sending {cmsg}".format(
+                                                            cmsg=classic_mssg))
+                sent_mssgs +=  classic_mssg
+
+                # receiving qu data frame
+                dcdd_mssg = receive_frame(host=Bob, qmem_itfc=qumem_itfc_B, 
+                                          Frame=SDC_frame)
+                
+                if VERBOSE:
+                    print("COMM - Bob received {cmsg}".format(cmsg=dcdd_mssg))
+                dcdd_mssgs += dcdd_mssg
+                
+            else: # generate epr frame and then consume it
+                epr_frame_counter += 1
+                if VERBOSE:
+                    print("COMM - On demand EPR generation\nAlice is sending EPR"
+                          " frame Nr:{ef}".format(ef=epr_frame_counter))
+                # sending and receiving epr_frame
+                EPR_frame = send_epr_frame(host=Alice, receiver_id=Bob.host_id, 
+                                           epr_gen=Alice_EPR_gen, 
+                                           qmem_itfc=qumem_itfc_A, error_gen=rot_error)
+                if VERBOSE:
+                    print("COMM - On demand EPR generation\nBob is receiving "
+                          "EPR frame Nr:{ef}".format(ef=epr_frame_counter)) 
+        
+                fest = receive_frame(host=Bob, qmem_itfc=qumem_itfc_B, Frame=EPR_frame)
+                qumem_itfc_A.set_F_est_EPR_END_PHASE(F_est=fest)
+
+                SDC_frame, classic_mssg = send_sdense_frame(host=Alice, 
+                                                        receiver_id=Bob.host_id,
+                                                        qmem_itfc=qumem_itfc_A,
+                                                        error_gen=rot_error)
+                if VERBOSE:
+                    print("COMM - Alice is sending {cmsg}".format(
+                                                            cmsg=classic_mssg))
+                sent_mssgs +=  classic_mssg
+                # receiving qu data frame
+                if VERBOSE:
+                    print("COMM - Bob is receiving Data frame"
+                          " Nr:{df}".format(df=data_frame_counter))
+            
+                dcdd_mssg = receive_frame(host=Bob, qmem_itfc=qumem_itfc_B, 
+                                          Frame=SDC_frame)
+
+                if VERBOSE:
+                    print("COMM - Bob received {cmsg}".format(cmsg=dcdd_mssg))
+                dcdd_mssgs += dcdd_mssg
+
+            if len(im_2_send) == 0:
+
+                finish_time = time.time()
+                print("Image was completely sent")
+                received_indexes = []
+                c1 = 0 
+                c2 = 0
+                for i in range(int(len(dcdd_mssgs)/2)):
+                    c1, c2 = tuple(map(int, dcdd_mssgs[i*2:i*2 +2]))
+                    rec_index = integerFromBinaryTuple(c1, c2)
+                    received_indexes.append(rec_index)
+
+                for row in range(rows):
+                    for column in range(coloumns):
+                        received_hash = hashes[received_indexes[0]]
+                        received_color = palette[received_hash]
+                        received_mario[column, row, :] = received_color
+                        received_indexes.pop(0)
+
+                received_im = Image.fromarray(received_mario)
+                
+                if save:
+                    received_im.save('./mario_link_varying_fid_and_channel.bmp')
+                break
+
+    print("Simulation time duration in seconds is: {t}".format(
+                                                    t=finish_time - start_time))
+    print("A total of {nrp} EPR pairs was generated".format(
+                                            nrp=epr_frame_counter*FRAME_LENGTH))
+
+    print("Alice sent the following classical bitstream:\n"
+          "{bstrm}".format(bstrm=sent_mssgs))
+
+    print("\nBob received the following classical bitstream:"
+          "\n{rbstrm}".format(rbstrm=dcdd_mssgs))
+    """
+    exp_num = 2
+
+    base_path = "./Playground_v0.8/erp_dist_link_direct_store_data/"
+
+
+    epr_hist_alice = base_path + "alice_epr_frame_history_exp_" + str(exp_num) + ".csv"
+    epr_hist_bob = base_path + "bob_epr_frame_history_exp_" + str(exp_num) + ".csv"
+
+    qumem_itfc_A.EPR_frame_history.to_csv(epr_hist_alice)
+    qumem_itfc_B.EPR_frame_history.to_csv(epr_hist_bob)
+
+    sdc_hist_alice = base_path + "alice_sdc_frame_history_exp_" + str(exp_num) + ".csv"
+    sdc_hist_bob = base_path + "bob_sdc_frame_history_exp_" + str(exp_num) + ".csv"    
+    
+    qumem_itfc_A.SDC_frame_history.to_csv(sdc_hist_alice)
+    qumem_itfc_B.SDC_frame_history.to_csv(sdc_hist_bob)
+
+    input_hist_alice = base_path + "alice_in_halves_history_exp_" + str(exp_num) + ".csv"
+    input_hist_bob = base_path + "bob_in_halves_history_exp_" + str(exp_num) + ".csv"
+    
+    qumem_itfc_A.In_halves_history.to_csv(input_hist_alice)
+    qumem_itfc_B.In_halves_history.to_csv(input_hist_bob)
+
+    """
+    ax = plt.gca()
+          
+    Alice_EPR_gen.history.plot(x="time_gauss", y="mu", kind="scatter", ax=ax)
+    rot_error.history.plot(x="time_gauss", y="mu", kind="scatter", color="red", ax=ax)
+    plt.show()
+
+    Alice_EPR_gen.history.plot(x="time_gauss", y="sig", kind="scatter")
+    plt.show()
+    rot_error.history.plot(x="time_gauss", y="sig", kind="scatter")
+    plt.show()
+
+    Alice_EPR_gen.history.plot(x="time_fid", y="fid", kind="scatter")
+    plt.show()
+    Alice_EPR_gen.history.plot(x="time_gen", y="fid_gen", kind="scatter")
+    plt.show()
+
+    rot_error.history.plot(x="time_gamm", y="gamm", kind="scatter")
+    plt.show()
+    
+    print("\nFinishing simulation!")
+    Alice.stop()
+    Bob.stop()
+    network.stop()    
+    
+
+if __name__=='__main__':
+    main()
---
+++ EPR_distribution_Link/No_Protocol_version(just_store_frames)/epr_gen_class.py
@@ -0,0 +1,250 @@
+from qunetsim.components import Host
+from qunetsim.objects import Qubit
+import pandas as pd
+import numpy as np 
+import time
+
+class EPRgenBadParameterException(Exception):
+    pass
+
+class HostNotStartedException(Exception):
+    pass
+
+class EPRgenBadTypException(Exception):
+    pass
+
+class EPR_generator(object):
+    def __init__(self, host:Host, max_fid=0.99, min_fid=0.5, max_dev=0.1, 
+                 min_dev=0.01, f_mu=None, f_sig=None, sig_phase=None, 
+                 spread=None, typ="sinusoidal"):
+
+        if host._queue_processor_thread is None:
+            raise HostNotStartedException("host must be started before"
+                                          " initializing its EPR generator.")
+        self.host = host
+        if max_fid < min_fid:
+            raise EPRgenBadParameterException("max_fid must be bigger than"
+                                              " min_fid.")
+        if (max_fid > 1) or (min_fid < 0.25):
+            raise EPRgenBadParameterException("max_fid must be less than or "
+                                              "equal to 1.\nmin_fid must be "
+                                              "greather than or equal to 0.25")
+        self.max_mu = max_fid
+        self.min_mu = min_fid
+        
+        if max_dev < min_dev:
+            raise EPRgenBadParameterException("max_dev must be bigger than"
+                                              " min_dev.")
+        if (max_dev > 1) or (min_dev < 0):
+            raise EPRgenBadParameterException("max_dev must be less than or "
+                                              "equal to 1.\nmin_dev must be "
+                                              "greather than or equal to 0")
+        self.max_sig = max_dev
+        self.min_sig = min_dev
+        if spread is None:
+            self.spread = 0.2
+        if (typ == "sinusoidal") or (typ == "gaussian"):
+            self.typ = typ
+            bias_mu_sin = (max_fid + min_fid) /2
+            bias_sig_sin = (max_dev + min_dev)/2
+            if self.typ == "sinusoidal":     
+                amp_mu_sin = (max_fid - min_fid)/2
+                amp_sig_sin = (max_dev - min_dev)/2
+                self.amp_mu_sin = amp_mu_sin
+                self.bias_mu_sin = bias_mu_sin
+                self.amp_sig_sin = amp_sig_sin
+                self.bias_sig_sin = bias_sig_sin
+                self.mu = None
+                self.sig = None
+                if f_mu is None:
+                    # EPR frame of 43 qubits take 5.2 seconds
+                    # we assume a default T = 20.8 s ~ f = 0.05 Hz for mean value
+                    self.wf_mu = np.pi / 10
+                else:
+                    self.wf_mu = 2*np.pi*f_mu
+
+                if f_sig is None:
+                    # we assume a default Tau = 2T ~ f = 0.1 Hz for standard deviation
+                    self.wf_sig = np.pi / 5
+                else:
+                    self.wf_sig = 2*np.pi*f_sig
+
+                if sig_phase is None:
+                    #we assume a default phase of pi/2 for the sigma sinusoidal
+                    self.phi_sig = np.pi / 2
+                else:
+                    self.phi_sig = sig_phase
+            else:
+                self.mu = bias_mu_sin
+                self.sig = bias_sig_sin
+                self.amp_mu_sin = None
+                self.bias_mu_sin = None
+                self.amp_sig_sin = None
+                self.bias_sig_sin = None
+                self.wf_mu = None
+                self.wf_sig = None
+                self.phi_sig = None
+        else:
+            raise EPRgenBadTypException("typ must be sinusoidal or gaussian, "
+                                        "{ntyp} is not implemented".format(ntyp=typ))
+        self.started = False
+        self._start_time = None
+        
+    def _start(self):
+        if self.typ == "sinusoidal":
+            self.history = pd.DataFrame({"time_gauss": pd.Series(dtype="float"),
+                                         "mu": pd.Series(dtype="float"),
+                                         "sig": pd.Series(dtype="float"),
+                                         "time_fid": pd.Series(dtype="float"),
+                                         "fid": pd.Series(dtype="float"),
+                                         "time_gen": pd.Series(dtype="float"),
+                                         "fid_gen": pd.Series(dtype="float")})
+        else:
+            self.history = pd.DataFrame({"time_fid": pd.Series(dtype="float"),
+                                         "fid": pd.Series(dtype="float"),
+                                         "time_gen": pd.Series(dtype="float"),
+                                         "fid_gen": pd.Series(dtype="float")})
+        self._start_time =  time.perf_counter()
+        self.started = True
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    def start(self):
+        self._start()
+
+    def _actualize_history(self, df_to_add):
+        if self.history.size == 0:
+            self.history = pd.concat([self.history, df_to_add])
+        else:
+            if self.history.iloc[-1].isnull().values.any():
+                boolcols = self.history.columns.isin(df_to_add.columns.values)
+                vals = df_to_add.values
+                vals = vals.reshape((vals.shape[1],))
+                self.history.iloc[-1, boolcols] = vals
+            else:
+                self.history = pd.concat([self.history, df_to_add])
+
+    def _mu_sig_sin(self):
+        t = time.perf_counter() - self.start_time
+        mu = self.amp_mu_sin * np.sin(self.wf_mu*t) + self.bias_mu_sin
+        sig = self.amp_sig_sin * np.sin(self.wf_sig*t + self.phi_sig) + self.bias_sig_sin
+        
+        df_gauss = pd.DataFrame([[t, mu, sig]], columns=["time_gauss", "mu", "sig"])
+        self._actualize_history(df_to_add=df_gauss)
+        return mu, sig
+
+    def _get_fidelity_4_gen(self):
+        if not self.started:
+            self._start() 
+        if self.typ == "sinusoidal":
+            mu, sig = self._mu_sig_sin()
+        else:
+            mu = self.mu
+            sig = self.sig
+        f = np.random.normal(loc=mu, scale=sig)
+        t = time.perf_counter() - self.start_time
+        df_fid = pd.DataFrame([[t, f]], columns=["time_fid", "fid"])
+        self._actualize_history(df_to_add=df_fid)
+        if f > 1:
+            f = 0.99
+        if f < 0.25:
+            f = 0.25
+        return f
+
+    def _fidelity(self, epr_half: Qubit):
+        """Fidelity between an imperfect EPR Pair and the bell state 
+        \ket{phi^{+}}, for an imperfect EPR Pair with state vector as follows
+        B1 = a_{1} + ib_{1}
+        B2 = a_{2} + ib_{2} 
+        B3 = a_{3} + ib_{3}
+        B4 = a_{4} + ib_{4}
+
+        The Fidelity is defined as 
+        F =  0.5((a_{1} + a_{4})^2 + (b_{1} + b_{4})^2)
+        """
+        epr_ket = epr_half.statevector()[1]
+
+        if np.shape(epr_ket)[0] == 4: # a valid epr_ket
+            b1 = epr_ket[0]
+            b4 = epr_ket[3]
+            fid = 0.5 * (((b1.real + b4.real)**2) + ((b1.imag + b4.imag)**2))
+            return fid
+        else: # invalid epr_ket
+            raise ValueError("Argument passed to epr_halve is not a two dimensional"
+                             " qubit system!")
+    
+    def get_fidelity(self, half: Qubit):
+        f = self._fidelity(epr_half=half)
+        return f
+
+    def _gen_epr(self, rx1=0, ry1=0, rx2=0, ry2=0):
+        """Generate an EPR-Pair by applying x and y rotational gates with angles 
+        rx1, ry1, rx2, and ry2 to the first and second qubit before passing them 
+        trough the entanglement circuit.
+        If default rotation angles are used a perfect EPR-Pair is generated."""
+
+        q1 = Qubit(self.host)
+        q2 = Qubit(self.host, q_id=q1.id)
+
+        # apply rotational gates 
+        q1.rx(phi=rx1)
+        q1.ry(phi=ry1)
+        q2.rx(phi=rx2)
+        q2.ry(phi=ry2)
+
+        # entanglement circuit
+        q1.H()
+        q1.cnot(q2)
+        gen_time = time.perf_counter() - self.start_time
+        gen_f = self._fidelity(epr_half=q1)
+        df_gen = pd.DataFrame([[gen_time, gen_f]], columns=["time_gen", "fid_gen"])
+        self._actualize_history(df_to_add=df_gen)
+
+        return q1, q2
+
+    def get_EPR_pair(self):
+        """ EPR-Pairs are generated by applying X and Y rotation gates to the zero 
+        ket qubits before entering the entanglement circuit. The rotation angles 
+        have the same value alpha which generate a EPR-Pair with fidelity F, which 
+        could drawn from sinusoidal mean and standard deviation value for the 
+        gaussian distribution(check _get_fidelity_4_gen) or from a simple gaussian 
+        distribution.
+        """
+        F = self._get_fidelity_4_gen()
+        alpha = 0.5*np.arccos(4*np.sqrt(F) - 3)
+        q1, q2 = self._gen_epr(rx1=alpha, ry1=alpha, rx2=alpha, ry2=alpha)
+        
+        return q1, q2
+        
+    def get_EPR_pair_gauss_angles(self):
+        F = self._get_fidelity_4_gen()
+        alpha = 0.5*np.arccos(4*np.sqrt(F) - 3)
+        sigma = alpha*self.spread
+        rxa, rya, rxb, ryb = np.random.normal(loc=alpha, scale=sigma, size=4)
+        q1, q2 = self._gen_epr(rx1=rxa, ry1=rya, rx2=rxb, ry2=ryb)
+
+        return q1, q2
+
+def EPR_Pair_fidelity(epr_halve: Qubit):
+    """Fidelity between an imperfect EPR Pair and the bell state 
+    \ket{phi^{+}}. For imperfect EPR Pair with state vector as follows
+    B1 = a_{1} + ib_{1}
+    B2 = a_{2} + ib_{2} 
+    B3 = a_{3} + ib_{3}
+    B4 = a_{4} + ib_{4}
+
+    The Fidelity is defined as 
+    F =  0.5((a_{1} + a_{4})^2 + (b_{1} + b_{4})^2)
+    """
+    epr_ket = epr_halve.statevector()[1]
+
+    if np.shape(epr_ket)[0] == 4: # a valid epr_ket
+        b1 = epr_ket[0]
+        b4 = epr_ket[3]
+        fid = 0.5 * (((b1.real + b4.real)**2) + ((b1.imag + b4.imag)**2))
+        return fid
+    else: # invalid epr_ket
+        raise ValueError("Argument passed to epr_halve is not a two dimensional"
+                         " qubit system!")
