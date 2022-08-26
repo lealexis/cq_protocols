2e7f00d5a60fee783673e18deca7c6870f4da7c2
lealexis <leonardo_alexis.gonzalez_zuniga@mailbox.tu-dresden.de>
2022-08-27 01:23:13 +0200

    pipelined classical channel debugged for events marking first and second feedback

--- EPR_distribution_Link/No_Protocol_version(just_store_frames)/channel_pipelines_class.py
+++ EPR_distribution_Link/No_Protocol_version(just_store_frames)/channel_pipelines_class.py
@@ -2,9 +2,6 @@ from queue import Queue
 from qunetsim.objects import Qubit
 from threading import Event, Timer 
 
-class NotStringException(Exception):
-    pass
-
 class OutSocketFull(Exception):
     pass
 
@@ -34,6 +31,7 @@ class QuPipe(object):
         if self.pipe_load == 0:
             self.Qframe_in_transmission.set()
         self.pipe_load += 1
+        #print(self.pipe_load)
         t_get = Timer(self._delay, self.get)
         self._queue.put(flying_qubit)
         t_get.start()
@@ -74,29 +72,28 @@ class ClassicPipe(object):
         self._delay = delay
     
     def put(self, cbit:str):
-        # TODO: check which type is the classic message to be sent is
-        if type(cbit) is str:
-            if (self.pipe_load == 0) and (self.feedback_num == 0):
-                self.fst_feedback_in_trans.set()
-                self.feedback_num += 1
-            if (self.pipe_load == 0) and (self.feedback_num == 1):
-                self.snd_feedback_in_trans.set()
-                self.feedback_num += 1
-            self.pipe_load += 1
-            t_get = Timer(self.delay, self.get)
-            self._queue.put(cbit)
-            t_get.start()
-        else:
-            raise NotStringException("Classic bit must be a string.")
+
+        if (self.pipe_load == 0) and (self.feedback_num == 0):
+            self.fst_feedback_in_trans.set()
+            self.feedback_num += 1
+        if not self.fst_feedback_in_trans.is_set() and (self.pipe_load == 0)\
+                                                 and (self.feedback_num == 1):
+            self.snd_feedback_in_trans.set()
+            self.feedback_num += 1
+        self.pipe_load += 1
+        t_get = Timer(self.delay, self.get)
+        self._queue.put(cbit)
+        t_get.start()
+
 
     def get(self):
         if len(self.out_socket) == 0:
-            self.out_socket.append(self._queue.get())
+            self.out_socket.append(self._queue.get()) 
             self.pipe_load -= 1
             if (self.pipe_load == 0) and (self.feedback_num == 1):
                 self.fst_feedback_in_trans.clear()
             if (self.pipe_load == 0) and (self.feedback_num == 2):
                 self.snd_feedback_in_trans.clear()
-                self.feedback_num = 0 
+                self.feedback_num = 0
         else:
             raise OutSocketFull("C-bit has not yet been taked from output socket.")
